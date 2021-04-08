87ebc8e8f1ae6e0ef469ffd0319ba99604ca6b97
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-04-08 14:07:37 +0300

    [LAB-9] Small user registration form fix

--- lab-4/src/main/java/com/brtvsk/lab4/model/auth/UserRegistrationForm.java
+++ lab-4/src/main/java/com/brtvsk/lab4/model/auth/UserRegistrationForm.java
@@ -5,18 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRegistrationForm {
-    @Length(min = 1, max = 20, message = "Login length must be from 1 to 20")
-    @Pattern(regexp = "^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\\d.-]{0,19}$", message = "Login must contain only latin letters and numbers")
+    @Length(min = 3, max = 20, message = "Login length must be from 3 to 20")
+    @Pattern(regexp = "^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\\d.-]{0,19}$", message = "Login must contain only latin letters and numbers and start with letter")
     private String login;
-    @NotBlank(message = "Password is mandatory")
+    @Length(min = 8, max = 20, message = "Password length must be from 8 to 20")
     private String password;
     @NotBlank(message = "Repeat password")
     private String passwordConfirm;
